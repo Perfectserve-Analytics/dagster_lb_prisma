228866d918b9f60b324896bfff87cd67915243f4
Cody <cmclean1213@gmail.com>
2023-04-28 13:14:45 -0400

    updates

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from setuptools import find_packages, setup
 
-setup(
-    name="prisma_lb",
-    packages=find_packages(exclude=["prisma_lb_tests"]),
-    install_requires=[
-        "dagster",
-        "dagster-cloud",
-        "dagster-dbt",
-        "dagster-snowflake",
-        "dbt-core",
-        "boto3",
-        "pandas",
-        "matplotlib",
-        "textblob",
-        "tweepy",
-        "wordcloud",
-    ],
-    extras_require={"dev": ["dagit", "pytest"]},
-)
+if __name__ == "__main__":
+    setup(
+        name="prisma_lb",
+        packages=find_packages(exclude=["prisma_lb_tests"]),
+        install_requires=[
+            "dagster",
+            "dagster-cloud",
+            "dagster-dbt",
+            "dagster-snowflake",
+            "dbt-core",
+            "pandas",
+            "matplotlib",
+        ],
+        extras_require={"dev": ["dagit", "pytest"]},
+    )
