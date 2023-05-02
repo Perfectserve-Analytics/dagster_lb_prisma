b7787ecc59b29d325c449651e742c02721c3db80
Cody <cmclean1213@gmail.com>
2023-05-02 11:17:51 -0400

    dagster snowflake

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
             "dagster-dbt",
             "dagster-snowflake",
             "dbt-core",
+            "dbt-snowflake"
             "pandas",
             "matplotlib",
         ],
