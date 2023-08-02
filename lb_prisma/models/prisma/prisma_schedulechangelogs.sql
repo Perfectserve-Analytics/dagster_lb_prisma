514a64e1b716345f5173a5ff3b30d22405a7c466
Cody <cmclean1213@gmail.com>
2023-08-02 12:00:12 -0400

    add schedulechangelogs

---
+++ lb_prisma/models/prisma/prisma_schedulechangelogs.sql
@@ -0,0 +1,11 @@
+select *
+
+from {{ source('lb_legacy_dbtemp_1_lblogs_dbo', 'schedulechangelogs') }}
+
+where customerid = 1140
+
+{% if is_incremental() %}
+
+where _FIVETRAN_SYNCED > (select max(_FIVETRAN_SYNCED) from {{ this }} )
+
+{% endif %}
