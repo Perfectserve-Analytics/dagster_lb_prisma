37b03ec570f1964d6ab47bab277b7da3c8d07706
Cody <cmclean1213@gmail.com>
2023-08-03 10:46:22 -0400

    schedulechangelogs fix

--- lb_prisma/models/prisma/prisma_schedulechangelogs.sql
+++ lb_prisma/models/prisma/prisma_schedulechangelogs.sql
@@ -6,6 +6,6 @@ where customerid = 1140
 
 {% if is_incremental() %}
 
-where _FIVETRAN_SYNCED > (select max(_FIVETRAN_SYNCED) from {{ this }} )
+and _FIVETRAN_SYNCED > (select max(_FIVETRAN_SYNCED) from {{ this }} )
 
 {% endif %}
